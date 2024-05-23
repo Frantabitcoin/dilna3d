3cf4c45949a7abfb5bf45e4a66de03b36009db67
Frantabitcoin <162628811+Frantabitcoin@users.noreply.github.com>
2024-05-23 14:53:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript functionality can be added here if needed
+    console.log('Website is loaded and ready.');
+});
+console.log("ahoj")
